19c0cf52125a1d0744d0311c2936a6b08d956638
Roy Lovitt <lovittl@my.erau.edu>
2018-01-22 11:43:00 -0500

    High level psudo code through view account and Main Menu Display Code written.

--- Mod3Ex3D/Program.cs
+++ Mod3Ex3D/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 
 namespace Mod3Ex3D
 {
@@ -6,7 +6,37 @@ namespace Mod3Ex3D
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Produce a two column accounting system that
+            //has 7 standard categories -- 
+            //assets, 
+            // user starts program
+            //user presented with 4 possible actions (main menu)
+         Console.WriteLine("MAIN MENU");
+         Console.WriteLine("____________________________________________");
+         Console.WriteLine("Please make a choice from the following list and hit enter");
+         Console.WriteLine("1)Check Balances, 2)View Account, 3)Enter Transaction, 4)View Reports, 5)Exit");
+
+            //1)check balances
+            //2)view account
+            //3)enter Transaction
+            //4)view reports
+            //start program
+            //user presented with 4 choices above
+            //user chooses 1
+            //balances of all accounts are displayed
+            //user is returned to a prompt presenting 4 choices as before
+            //user choosed 2
+            //user is presented with the list of 7 accounts
+            //user selects account
+            //account information is displayed
+            //return to main menu
+
+
+
+
+            //Console.WriteLine = int.parse(Console.Read("cats"));
+
+            //Console.WriteLine("Hello World!");
         }
     }
 }
