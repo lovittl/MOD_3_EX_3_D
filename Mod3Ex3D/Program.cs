d2312944247929df0d3f0114f7915cd8e1ef51aa
Roy Lovitt <lovittl@my.erau.edu>
2018-01-23 11:23:09 -0500

    Completed Menu loop (and created methods outside of Main to displayMenu and collect user selection).

--- Mod3Ex3D/Program.cs
+++ Mod3Ex3D/Program.cs
@@ -1,4 +1,4 @@
-﻿ using System;
+﻿using System;
 
 namespace Mod3Ex3D
 {
@@ -6,37 +6,92 @@ namespace Mod3Ex3D
     {
         static void Main(string[] args)
         {
-            //Produce a two column accounting system that
-            //has 7 standard categories -- 
-            //assets, 
-            // user starts program
-            //user presented with 4 possible actions (main menu)
-         Console.WriteLine("MAIN MENU");
-         Console.WriteLine("____________________________________________\n");
-         Console.WriteLine("Please make a choice from the following list and hit enter");
-         Console.WriteLine("1)Check Balances, 2)View Account, 3)Enter Transaction, 4)View Reports, 5)Exit");
-            Console.Read();
-            //1)check balances
-            //2)view account
-            //3)enter Transaction
-            //4)view reports
-            //start program
-            //user presented with 4 choices above
-            //user chooses 1
-            //balances of all accounts are displayed
-            //user is returned to a prompt presenting 4 choices as before
-            //user choosed 2
-            //user is presented with the list of 7 accounts
-            //user selects account
-            //account information is displayed
-            //return to main menu
+            int selection = 0;
+            do
+            {
+                displayMenu();
+                selection = readInt();
+            }
 
+            while (selection != 5);
 
-
-
-            //Console.WriteLine = int.parse(Console.Read("cats"));
-
-            //Console.WriteLine("Hello World!");
         }
+
+        private static void displayMenu()
+        {
+
+            Console.WriteLine("MAIN MENU");
+            Console.WriteLine("____________________________________________\n");
+            Console.WriteLine("Please make a choice from the following list and hit enter");
+            Console.WriteLine("1)Check Balances, 2)View Account, 3)Enter Transaction, 4)View Reports, 5)Exit");
+
+        }
+
+        private static int readInt()
+        {
+            return int.Parse(Console.ReadLine());
+
+        }
+
+
+
+
+
+        //******************************GARBAGE****************************************************
+
+        // int getMenuNumber =  
+        //Produce a two column accounting system that
+        //has 7 standard categories -- 
+        //assets, 
+        // user starts program
+        //user presented with 4 possible actions (main menu)
+        /*   int getMenuNumber =
+           Console.Read();
+
+           //switch;
+        */
+        //1)check balances
+        // Console.WriteLine("Check Balances");
+        //user selects 1 and presses enter.  
+        //all balances are displayed
+        //user is returned to the Main Menu
+        /*   2)view account
+                  screen is cleared in case other functions had been called previously
+
+                  a sql query is sent to the database and a view is displayed to the user 
+                  containing all the data elements of an account. 
+                  ?are all elements from the same table or multiple tables?
+            */
+        // ClearConsole ClearMyConsole = new ClearConsole();
+
+        //3)enter Transaction
+        //4)view reports
+        //start program
+        //user presented with 4 choices above
+        //user chooses 1
+        //balances of all accounts are displayed
+        //user is returned to a prompt presenting 4 choices as before
+        //user choosed 2
+        //user is presented with the list of 7 accounts
+        //user selects account
+        //account information is displayed
+        //return to main menu
+
+
+
+
+        //Console.WriteLine = int.parse(Console.Read("cats"));
+
+        //Console.WriteLine("Hello World!");
+
+
+
+
+
+
+
+
     }
 }
+
+
