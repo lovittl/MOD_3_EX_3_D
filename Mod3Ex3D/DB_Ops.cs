89b09c687780c4bf14d7a26939e91af1ba53ca5c
Roy Lovitt <lovittl@my.erau.edu>
2018-01-28 08:55:09 -0500

    insert comments in the DB_Ops Class explaining its purpose and goal upon implentation.

--- Mod3Ex3D/DB_Ops.cs
+++ Mod3Ex3D/DB_Ops.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+/* This is an empty Class where I am planning to implement the data base creation and methods 
+ * that will insert and retrieve data as well as perform insertions into tables that will 
+ * represent debits and credits and perform balancing to represent the The accounting equation, 
+ * Assets = Equity + Liabilities.  
+ */
 namespace Mod3Ex3D
 {
     class DB_Ops
